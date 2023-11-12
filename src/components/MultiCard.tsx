a7539f2a12c47e6cc8276bdf4da3d15ed1cf9177
Johnny Vasil <jfvasil@gmail.com>
2023-11-12 18:13:02 -0500

    fixed error with build

--- src/components/MultiCard.tsx
+++ src/components/MultiCard.tsx
@@ -1,17 +1,8 @@
 
-import Card from "./Card"
 
 const MultiCard = () => {
-
-
-
   return (
- <div className="flex flex-col">
-    <Card number={1} />
-    <Card number={2} />
-    <Card number={3} />
-    <Card number={4} />
- </div>
+    <div>MultiCard</div>
   )
 }
 
