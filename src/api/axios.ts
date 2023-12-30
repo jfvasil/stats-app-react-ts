0f047e06d2cd1300229549565bc93f5fad0e8b7b
Johnny Vasil <jfvasil@gmail.com>
2023-12-29 19:10:56 -0500

    changed axios base url

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -1,7 +1,8 @@
 import axios from 'axios'
 
 // const BASE_URL = 'http://localhost:3000'
-const BASE_URL = 'http://3.15.0.99:8080/'
+// const BASE_URL = 'http://3.15.0.99:8080/'
+const BASE_URL = 'https://api.stats-app-backend.xyz'
 
 export const axiosInstance = axios.create({
     baseURL: BASE_URL
